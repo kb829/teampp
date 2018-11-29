be70b6ae8c0ee8731cba80107da996015ab5780f
biny <kbpark829@gmail.com>
2018-11-29 19:15:12 +0900

    add login

--- app_file.js
+++ app_file.js
@@ -1,4 +1,5 @@
-var express = require('express');
+const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -6,4 +7,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/logInpage'))
+  .get('/time',(req, res)=>res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  showTimes = () => {
+    let result =''
+    const times = process.env.TIMES || 5
+    for(i=0;i<times;i++){
+      result+=i+' '
+    }
+    return result;
+  }
