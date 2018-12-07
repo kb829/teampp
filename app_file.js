0178b487f16fb62f97a7464c93582f5165ff1ade
biny <kbpark829@gmail.com>
2018-12-07 18:10:24 +0900

    change js

--- app_file.js
+++ app_file.js
@@ -4,36 +4,11 @@ const bodyParser=require('body-parser')
 const PORT = process.env.PORT || 5000
 var session=require('express-session')
 var app=express()
-var mongoose=require('mongoose');
-var MongoStore=require('connect-mongo')(session)
-var MongoClient = require('mongodb').MongoClient
-var schema=require("./schema");
-var mongoUrl="mongodb://localhost:27017/data"
-var bcrypt = require("bcrypt-nodejs");
 //multer
 var multer = require('multer');
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-//var urlencodedParser = bodyParser.urlencoded({extended: false})
-
-// MongoClient.connect('mongodb://localhost:27017',
-//   (err, client)=> {
-//     if(err) throw err;
-//     var db=client.db('data');
-//     console.log(db);
-//     //client.close();
-// });
-// const pool=new Pool({
-//   connectionString: process.env.postgresql-cylindrical-95619,
-//   ssl: true
-// });
-var db=mongoose.connection;
-db.on('error',console.error);
-db.once('open',function(){
-  console.log("Connected to mongod server");
-})
-mongoose.connect('mongodb://localhost:27017/data');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
@@ -41,11 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
   secret:'1234DSFs@adf1234!@#$asd',
   resave: false,
-  saveUninitialized: true,
-  store:new MongoStore({
-    url:mongoUrl,
-    ttl:60*60*24*7
-  })
+  saveUninitialized: true
 }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -54,8 +25,7 @@ app.get('/logInpage',(req,res)=>res.render('pages/logInpage'))
 app.get('/time',(req, res)=>res.send(showTimes()))
 app.get('/signUp',(req, res)=>res.render('pages/signUp'))
 app.get('/popup_Proj',(req, res)=>res.render('pages/popup_Proj'))
-app.get('/project',(req, res)=>res.render('pages/project')) 
-var Proj=mongoose.model('Proj',schema.projSchema);
+app.get('/project',(req, res)=>res.render('pages/project'))
 app.post('/popupProj',function(req,res){
   console.log("Project name : ", req.body.Project_name);
   var userproj=new Proj({
@@ -68,64 +38,20 @@ app.post('/popupProj',function(req,res){
     console.dir(userproj);
   })
 })
-// app.get('/auth/login',function(req,res){
-//   // if(req.session.userID=="1234" && req.session.userPW=="1234"){
-//   //   req.render('pages/mainframe')
-//   // }
-//   // else{
-//   //   req.render('pages/logInpage')
-//   // }
-//   if(req.session.count){
-//     req.session.count++;
-//   }else{
-//     req.session.count=1;
-//   }
-//   res.send('count : '+req.session.count);
-// })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var User=mongoose.model('User',schema.userSchema);
 app.post('/logInReceiver', function (req, res){
   console.log("ID : ", req.body.userID)
   console.log("PW : ", req.body.userPW)
   var uid=req.body.userID;
   var pwd=req.body.userPW;
-  User.findOne({'id':uid}).exec(function(err,user){
-    console.log(user+"\n");
-    if(user==null){
-      res.redirect('/logInpage');
-    }
-    else{
-    bcrypt.compare(pwd,user.password,function(err,ret){
-      if(err){
-        console.log('bcrypt compare error : ',err.message);
-      }else{
-        console.log(ret);
-        if(ret){
-          //res.render('pages/mainframe',{chk:'0',name:user.name});
-          var yname=user.name;
-          req.session.userName=yname;
-          req.session.userkey=user._id;
-          req.session.userdb=user;
-          Proj.find({"user":{$elemMatch:{"_id":req.session.userkey}}}).exec(function(err,projList){
-            if(err) return console.log(err);
-            console.log(projList+"\n");
-            if(projList==null){
-              res.render("pages/project",{name:req.session.userName, pList:'0'});
-            }
-            else{
-              res.render("pages/project",{name:req.session.userName, pList: projList});
-            }
-          })
-        }
-        else{
-          res.redirect('/logInpage');
-        }
-      }
-    })
+  req.session.userID=uid;
+  if(req.body.signID=="admin" && req.body.signPW=="404"){
+    res.render('pages/adminMain',{name:req.session.userID});
   }
-  })
-  //res.render('pages/mainframe')
+  else{
+    res.render('pages/mainframe', {chk:'0', name:req.session.userID});
+  }
 })
 app.post('/signUpReceiver', function (req, res){
   console.log("ID : ", req.body.signID)
@@ -133,33 +59,14 @@ app.post('/signUpReceiver', function (req, res){
   console.log("chkPw : ", req.body.checkPW)
   console.log("Name : ", req.body.signName)
   console.log("E mail : ",req.body.signEmail)
-  var userdata=new User({
-    id:req.body.signID,
-    password:req.body.signPW,
-    name:req.body.signName
-  })
-  userdata.save(function(err,userdata){
-    if(err) return console.error(err);
-    console.dir(userdata);
-  })
+
   res.render('pages/logInpage')
 })
 app.get('/selectProjRe',function(req,res){
-  console.log("here");
-  Proj.find({"user":{$elemMatch:{"_id":req.session.userkey}}}).exec(function(err,projList){
-    if(err) return console.log(err);
-    var pList1=projList;
-    console.log(pList1+"\n");
-    if(pList1==null){
-      res.render("pages/project",{name:req.session.userName, pList:'0'});
-    }
-    else{
-      res.render("pages/project",{name:req.session.userName, pList:pList1});//pList1});
-    }
-  })
+  res.render("pages/project.ejs",{name:req.session.userID});
 })
 app.get('/mainframe',function (req,res){
-  res.render("pages/mainframe",{chk:'0', name:req.session.userName});
+  res.render("pages/mainframe",{chk:'0', name:req.session.userID});
 })
 app.get('/manageVerRe',function(req,res){
   res.render("pages/mainframe",{chk:'1'});
@@ -195,36 +102,6 @@ app.post('/uploadFile',upload.single('userFile'),function(req,res){
   console.log(req.file);
   res.send('Uploaded:'+req.file.filename);
 })
-// var myController=(req,res)=>{
-//   var filename='myFile.ext';
-//   var absPath=path.join(__dirname,'uploads/',filename);
-//   var relPath=path.join('')
-// }
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/logInpage'))
-//   .get('/time',(req, res)=>res.send(showTimes()))
-//   .get('/db',async(req, res)=>{
-//     try{
-//       const client = await pool.connect()
-//       const result = await client.query('SELECT * FROM test_table');
-//       const result = {'results': (result) ? result.rows : null};
-//       res.render('pages/db',results);
-//       client.release();
-//     } catch(err){
-//       console.error(err);
-//       res.send("Error "+err);
-//     }
-//   })
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-//   .get('/form_receiver', (req, res)=>{
-//     var userID = req.query.userID;
-//     var userPW = req.query.userPW;
-//     res.send(userID+' '+userPW);
-//   })
   
 showTimes = () => {
   let result =''
